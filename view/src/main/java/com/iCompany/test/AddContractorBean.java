1daafb7c2e335c3fee39c978eac22673cd6a4334
Karol Dzięgiel <vercevales@gmail.com>
2016-02-16 00:13:00 +0100

    addContractor began work in progress

---
+++ view/src/main/java/com/iCompany/test/AddContractorBean.java
@@ -0,0 +1,17 @@
+package com.iCompany.test;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * Created by Karol Dziegiel on 15.02.16.
+ */
+@ManagedBean(name = "addContractor")
+@ViewScoped
+public class AddContractorBean {
+    private String companyNamy;
+    private String shortCoompanyName;
+    private String address;
+
+
+}
